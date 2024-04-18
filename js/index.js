b9efff1cecd3f345a1715a38926786a97077022a
ADEILTON PEREIRA ALVES <70703098+AdeiltonPereiraAlves@users.noreply.github.com>
2024-04-18 20:21:26 -0300

    Consertando responsividade e carrinho

---
+++ js/index.js
@@ -0,0 +1,19 @@
+
+    // pegando id do html
+    const carrinho = document.querySelector('#carrinho')
+    const butaoFecharcarrinho = document.querySelector('#botaoFecharCarrinho')
+    const closeCarrinho = document.querySelector('#fechar-carrinho')
+    
+
+    //abrindo e fechando o carrinho
+    carrinho.addEventListener('click', () => {
+        
+        closeCarrinho.classList.contains('hidden')?  closeCarrinho.classList.toggle('flex') && closeCarrinho.classList.remove('hidden'): closeCarrinho.classList.toggle('hidden')  && closeCarrinho.classList.remove('flex');
+      
+       
+    })
+    //fechando o carrinho pelo icone
+    butaoFecharcarrinho.addEventListener('click',() => {
+        closeCarrinho.classList.contains('hidden')?  closeCarrinho.classList.toggle('flex') && closeCarrinho.classList.remove('hidden'): closeCarrinho.classList.toggle('hidden')  && closeCarrinho.classList.remove('flex');
+        
+    })
