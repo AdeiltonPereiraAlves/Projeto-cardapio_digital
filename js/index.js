be93a4d9e1a555d5d5d0bcd91ce01eeff7b82945
ADEILTON PEREIRA ALVES <70703098+AdeiltonPereiraAlves@users.noreply.github.com>
2024-04-23 20:48:45 -0300

    consertando scroll x

--- js/index.js
+++ js/index.js
@@ -1,210 +1,231 @@
 //imports 
 
 // pegando id do html
-
-
-    let precoTotalCart = 0;
-    let numeroItemsCart = 0;
-    const carrinho = document.querySelector('#carrinho')
-    const butaoFecharcarrinho = document.querySelector('#botaoFecharCarrinho')
-    const closeCarrinho = document.querySelector('#fechar-carrinho')
-    const menu = document.querySelector('.menu-container')
-    const icon = document.querySelector('.icon')
+document.addEventListener('DOMContentLoaded', () => {
     
-    icon.textContent = numeroItemsCart;
+    
+let precoTotalCart = 0;
+let numeroItemsCart = 0;
+const carrinho = document.querySelector('#carrinho')
+const butaoFecharcarrinho = document.querySelector('#botaoFecharCarrinho')
+const closeCarrinho = document.querySelector('#fechar-carrinho')
+const menu = document.querySelector('.menu-container')
+const icon = document.querySelector('.icon')
 
-    //abrindo e fechando o carrinho
-    carrinho.addEventListener('click', () => {
-        if (closeCarrinho) {
+icon.textContent = numeroItemsCart;
 
-            closeCarrinho.classList.contains('hidden') ? closeCarrinho.classList.toggle('flex') && closeCarrinho.classList.remove('hidden') : closeCarrinho.classList.toggle('hidden') && closeCarrinho.classList.remove('flex');
-        }
+//abrindo e fechando o carrinho
+carrinho.addEventListener('click', () => {
+    if (closeCarrinho) {
 
-
-    })
-    //fechando o carrinho pelo icone
-    butaoFecharcarrinho.addEventListener('click', () => {
         closeCarrinho.classList.contains('hidden') ? closeCarrinho.classList.toggle('flex') && closeCarrinho.classList.remove('hidden') : closeCarrinho.classList.toggle('hidden') && closeCarrinho.classList.remove('flex');
-
-    })
-
+    }
 
 
-    //item do menu
-    const btnItem = document.querySelector('#btn-item')
+})
+//fechando o carrinho pelo icone
+butaoFecharcarrinho.addEventListener('click', () => {
+    closeCarrinho.classList.contains('hidden') ? closeCarrinho.classList.toggle('flex') && closeCarrinho.classList.remove('hidden') : closeCarrinho.classList.toggle('hidden') && closeCarrinho.classList.remove('flex');
 
-    menu.addEventListener('click', (event) => {
-        const parentBtn = event.target.closest('.btn-to-cart')
-        if (parentBtn) {
-            const id = parentBtn.getAttribute("data-id")
-            const nome = parentBtn.getAttribute("data-name")
-            const preco = parseFloat(parentBtn.getAttribute("data-price"))
-            const img = parentBtn.getAttribute("data-img")
-            addCart(id, nome, preco, img)
-
-        }
-    })
+})
 
 
-    // add cart
-    let arryCart = []
-    function addCart(id, nome, preco, img) {
-        const item = {
-            id: id,
-            nome: nome,
-            preco: preco,
-            img: img,
-            qnt: 1
-        }
-        if (!arryCart.some(item => item.id === id)) {
-            arryCart.push(item)
-            precoTotalCart = totalCart(arryCart);
-            numeroItemsCart ++;
+
+//item do menu
+const btnItem = document.querySelector('#btn-item')
+
+menu.addEventListener('click', (event) => {
+    const parentBtn = event.target.closest('.btn-to-cart')
+    if (parentBtn) {
+        const id = parentBtn.getAttribute("data-id")
+        const nome = parentBtn.getAttribute("data-name")
+        const preco = parseFloat(parentBtn.getAttribute("data-price"))
+        const img = parentBtn.getAttribute("data-img")
+        addCart(id, nome, preco, img)
+
+    }
+})
+
+
+// add cart
+let arryCart = []
+function addCart(id, nome, preco, img) {
+    const item = {
+        id: id,
+        nome: nome,
+        preco: preco,
+        img: img,
+        qnt: 1
+    }
+    if (!arryCart.some(item => item.id === id)) {
+        arryCart.push(item)
+        precoTotalCart = totalCart(arryCart);
+        numeroItemsCart++;
+        icon.textContent = numeroItemsCart;
+
+    }
+    else {
+        const itemNoCarrinho = arryCart.find(item => item.id === id);
+        if (itemNoCarrinho) {
+            // Se o item foi encontrado, aumente a quantidade
+
+            window.alert("O item ja esta no carrinho")
+
+        } else {
+            // Se o item não foi encontrado, adicione-o ao carrinho
+            arryCart.push({ id: id, qnt: qnt + 1 }); // Adiciona o item com quantidade 1 ao carrinho
+            numeroItemsCart++;
             icon.textContent = numeroItemsCart;
-
         }
-        else {
-            const itemNoCarrinho = arryCart.find(item => item.id === id);
-            if (itemNoCarrinho) {
-                // Se o item foi encontrado, aumente a quantidade
-
-                window.alert("O item ja esta no carrinho")
-              
-            } else {
-                // Se o item não foi encontrado, adicione-o ao carrinho
-                arryCart.push({ id: id, qnt: qnt + 1 }); // Adiciona o item com quantidade 1 ao carrinho
-                numeroItemsCart ++;
-                icon.textContent = numeroItemsCart;
-            }
-        }
-        listarCarrinho(arryCart)
-        console.log(arryCart)
-        totalPreco.textContent = `${precoTotalCart}`;
-        totalPrecoMenu.textContent = `${precoTotalCart}`;
-
     }
+    listarCarrinho(arryCart)
+    console.log(arryCart)
+    totalPreco.textContent = `${precoTotalCart}`;
+    totalPrecoMenu.textContent = `${precoTotalCart}`;
 
-    //preço total do carrinho
+}
 
-    function totalCart(cart) {
-        const total = cart.reduce((ac, item) => {
-            const precoTotal = item.preco * item.qnt;
+//preço total do carrinho
 
-            return ac + precoTotal;
+function totalCart(cart) {
+    const total = cart.reduce((ac, item) => {
+        const precoTotal = item.preco * item.qnt;
 
-        }, 0)
-        return total;
-    }
+        return ac + precoTotal;
+
+    }, 0)
+    return total;
+}
 
 
-    //listar itens do carrinho
-    const itemContainer = document.querySelector('.item-container')
-    const totalPreco = document.querySelector('#total-carrinho')
-    const totalPrecoMenu = document.querySelector('.total-carrinho')
+//listar itens do carrinho
+const itemContainer = document.querySelector('.item-container')
+const totalPreco = document.querySelector('#total-carrinho')
+const totalPrecoMenu = document.querySelector('.total-carrinho')
 
-    function listarCarrinho(cart) {
-        itemContainer.innerHTML = '';
-        cart.forEach(item => {
-            const itemDiv = document.createElement('div');
-            itemDiv.classList.add('flex', 'w-60', 'h-28', 'bg-slate-100', 'rounded-md', 'p-4', 'overflow-hidden','border-b');
+function listarCarrinho(cart) {
+    itemContainer.innerHTML = '';
+    cart.forEach(item => {
+        const itemDiv = document.createElement('div');
+        itemDiv.classList.add('flex', 'w-60', 'h-28', 'bg-slate-100', 'rounded-md', 'p-4', 'overflow-hidden', 'border-b');
 
-            const img = document.createElement('img');
-            img.src = item.img;
-            img.alt = '';
-            img.classList.add('w-14', 'h-14');
+        const img = document.createElement('img');
+        img.src = item.img;
+        img.alt = '';
+        img.classList.add('w-14', 'h-14');
 
-            const infoDiv = document.createElement('div');
-            infoDiv.classList.add('flex', 'flex-col', 'p-5', 'items-center', 'justify-center');
+        const infoDiv = document.createElement('div');
+        infoDiv.classList.add('flex', 'flex-col', 'p-5', 'items-center', 'justify-center');
 
-            const title = document.createElement('h1');
-            title.classList.add('font-black');
-            title.textContent = item.nome;
+        const title = document.createElement('h1');
+        title.classList.add('font-black');
+        title.textContent = item.nome;
 
-            const priceSpan = document.createElement('span');
-            priceSpan.classList.add('font-black');
-            priceSpan.textContent = `preço: ${item.preco}R$`;
+        const priceSpan = document.createElement('span');
+        priceSpan.classList.add('font-black');
+        priceSpan.textContent = `preço: ${item.preco}R$`;
 
-            const quantityDiv = document.createElement('div');
-            quantityDiv.classList.add('flex', 'p-2', 'gap-2');
+        const quantityDiv = document.createElement('div');
+        quantityDiv.classList.add('flex', 'p-2', 'gap-2');
 
-            const decreaseBtn = document.createElement('button');
-            decreaseBtn.classList.add('p-4','rounded-sm','border','flex','items-center','hover:bg-white','h-4','border-gray-800-dark')
-            decreaseBtn.textContent = '-';
-            decreaseBtn.addEventListener('click', () => {
-                if (item.qnt > 1) {
-                    item.qnt--;
-                    // Atualizar a exibição da quantidade na interface do usuário
-                    quantitySpan.textContent = item.qnt;
-                    precoTotalCart = totalCart(arryCart)
-
-                    totalPreco.textContent = `${precoTotalCart}`;
-                    totalPrecoMenu.textContent = `${precoTotalCart}`;
-                }
-            });
-
-            const quantitySpan = document.createElement('span');
-            quantitySpan.textContent = item.qnt;
-
-            const increaseBtn = document.createElement('button');
-            increaseBtn.classList.add('p-4','rounded-sm','border','flex','items-center','hover:bg-white','h-4','border-gray-800-dark')
-            increaseBtn.textContent = '+';
-            increaseBtn.addEventListener('click', () => {
-                item.qnt++;
+        const decreaseBtn = document.createElement('button');
+        decreaseBtn.classList.add('p-4', 'rounded-sm', 'border', 'flex', 'items-center', 'hover:bg-white', 'h-4', 'border-gray-800-dark')
+        decreaseBtn.textContent = '-';
+        decreaseBtn.addEventListener('click', () => {
+            if (item.qnt > 1) {
+                item.qnt--;
                 // Atualizar a exibição da quantidade na interface do usuário
                 quantitySpan.textContent = item.qnt;
-                
                 precoTotalCart = totalCart(arryCart)
 
                 totalPreco.textContent = `${precoTotalCart}`;
                 totalPrecoMenu.textContent = `${precoTotalCart}`;
-            });
-
-            quantityDiv.append(decreaseBtn, quantitySpan, increaseBtn);
-            infoDiv.append(title, priceSpan, quantityDiv);
-           
-            itemDiv.append(img, infoDiv);
-            itemContainer.appendChild(itemDiv)
-         
-            
-            
-           
-
-
+            }
         });
 
-        
+        const quantitySpan = document.createElement('span');
+        quantitySpan.textContent = item.qnt;
+
+        const increaseBtn = document.createElement('button');
+        increaseBtn.classList.add('p-4', 'rounded-sm', 'border', 'flex', 'items-center', 'hover:bg-white', 'h-4', 'border-gray-800-dark')
+        increaseBtn.textContent = '+';
+        increaseBtn.addEventListener('click', () => {
+            item.qnt++;
+            // Atualizar a exibição da quantidade na interface do usuário
+            quantitySpan.textContent = item.qnt;
+
+            precoTotalCart = totalCart(arryCart)
+
+            totalPreco.textContent = `${precoTotalCart}`;
+            totalPrecoMenu.textContent = `${precoTotalCart}`;
+        });
+
+        quantityDiv.append(decreaseBtn, quantitySpan, increaseBtn);
+        infoDiv.append(title, priceSpan, quantityDiv);
+
+        itemDiv.append(img, infoDiv);
+        itemContainer.appendChild(itemDiv)
+
+
+
+
+
+
+    });
+
+
+}
+
+//modal de pedidos e pagamento
+
+const btnPedidos = document.querySelector('#btn-pedidos')
+const pagContainer = document.querySelector('.pag-container')
+
+btnPedidos.addEventListener('click', () => {
+    closeCarrinho.classList.contains('hidden') ? closeCarrinho.classList.toggle('flex') && closeCarrinho.classList.remove('hidden') : closeCarrinho.classList.toggle('hidden') && closeCarrinho.classList.remove('flex');
+    
+    pagContainer.classList.contains('hidden') ? pagContainer.classList.toggle('flex') && pagContainer.classList.remove('hidden') : ""
+    if(pagContainer.classList.contains('flex')){
+        const btn = document.querySelector('#td-total')
+        btn.textContent = precoTotalCart;
+        listarTabela(arryCart)
+
     }
 
-    //modal de pedidos e pagamento
-    
-    const btnPedidos = document.querySelector('#btn-pedidos')
-    const pagContainer = document.querySelector('.pag-container')
-    
-    btnPedidos.addEventListener('click',() =>{
-        closeCarrinho.classList.contains('hidden') ? closeCarrinho.classList.toggle('flex') && closeCarrinho.classList.remove('hidden') : closeCarrinho.classList.toggle('hidden') && closeCarrinho.classList.remove('flex');
- 
-       pagContainer.classList.contains('hidden')? pagContainer.classList.toggle('flex') && pagContainer.classList.remove('hidden'):""
+
+})
+const butaoFecharModal = document.querySelector('#botaoFecharModal')
+
+butaoFecharModal.addEventListener('click', () => {
+    pagContainer.classList.toggle('hidden') && pagContainer.classList.remove('flex')
+    tabela.innerHTML = ''
+})
+
+//tabela do modal de pagamentos
+const tabela = document.querySelector('#tbody')
+function listarTabela(cart) {
+    cart.map((item) => {
        
-       const btn = document.querySelector('#td-total')
-       btn.textContent = precoTotalCart;
-
+        tabela.innerHTML += `
+            <tr>
+            <td class="w-1/2 border border-gray-300 px-4 py-2">${item.nome}<span> x ${item.qnt}</span></td>
+            <td class="w-1/2 border border-gray-300 px-4 py-2">${item.preco}</td>
+        </tr>
+            `
 
     })
-    const butaoFecharModal = document.querySelector('#botaoFecharModal')
-
-    butaoFecharModal.addEventListener('click', () => {
-        pagContainer.classList.toggle('hidden') && pagContainer.classList.remove('flex') 
-         
-    })
-
-    //pagina de pagamento
-    
-    
-
-
-  
-    
+}
 
 
 
+
+
+
+
+
+
+
+
+
+});
 
